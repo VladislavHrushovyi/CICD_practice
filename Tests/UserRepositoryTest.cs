f10324fedc114f16987c10b21c6b56f6d07b05d8
vladg <vlad.grush.99@gmail.com>
2024-02-14 17:11:16 +0200

    trying to mock testing

---
+++ Tests/UserRepositoryTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using CICD.Application.Features.AddNewUser;
+using CICD.Application.Repository;
+using CICD.Domain.Models;
+using Telerik.JustMock;
+
+namespace Tests;
+
+public class UserRepositoryTest
+{
+    [Fact]
+    public async Task CreateUserTest()
+    {
+        var dateString = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+        var addUser = new User()
+        {
+            Id = "1",
+            Name = "Vlad",
+            Surname = "Allo",
+            CreatedAt = dateString,
+            Cars = new List<Car>()
+        };
+        var mockUserRepository = Mock.Create<IUserRepository>();
+        Mock.Arrange( () => mockUserRepository.AddNewUser(addUser))
+            .Returns((User user) => user);
+        var addUserHandler = new AddNewUserHandler(mockUserRepository);
+        var result = await addUserHandler.Handle(new AddNewUserRequest()
+        {
+            Name = "Vlad",
+            Surname = "Allo"
+        });
+        Assert.NotNull(result);
+        Assert.Equal("1", result.Id);
+    }
+}
