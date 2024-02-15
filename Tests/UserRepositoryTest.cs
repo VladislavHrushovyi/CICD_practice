a7ae0dac87ee21f1f135ed2f203dfeb0edabc76f
vladg <vlad.grush.99@gmail.com>
2024-02-15 16:20:46 +0200

    first my mock tests

--- Tests/UserRepositoryTest.cs
+++ Tests/UserRepositoryTest.cs
@@ -1,8 +1,9 @@
 ﻿using System.Globalization;
 using CICD.Application.Features.AddNewUser;
+using CICD.Application.Features.GetUserById;
 using CICD.Application.Repository;
 using CICD.Domain.Models;
-using Telerik.JustMock;
+using Moq;
 
 namespace Tests;
 
@@ -11,25 +12,41 @@ public class UserRepositoryTest
     [Fact]
     public async Task CreateUserTest()
     {
-        var dateString = DateTime.Now.ToString(CultureInfo.InvariantCulture);
-        var addUser = new User()
-        {
+        var userAdd = new User()
+        {   
             Id = "1",
-            Name = "Vlad",
-            Surname = "Allo",
-            CreatedAt = dateString,
+            Surname = "vlad",
+            Name = "vlad",
+            CreatedAt = DateTime.Now.ToString(CultureInfo.InvariantCulture),
             Cars = new List<Car>()
         };
-        var mockUserRepository = Mock.Create<IUserRepository>();
-        Mock.Arrange( () => mockUserRepository.AddNewUser(addUser))
-            .Returns((User user) => user);
-        var addUserHandler = new AddNewUserHandler(mockUserRepository);
+        var userRepositoryMock = new Mock<IUserRepository>();
+        userRepositoryMock.Setup(x => x.AddNewUser(It.IsAny<User>()))
+            .ReturnsAsync(userAdd);
+
+        var addUserHandler = new AddNewUserHandler(userRepositoryMock.Object);
+
         var result = await addUserHandler.Handle(new AddNewUserRequest()
         {
-            Name = "Vlad",
-            Surname = "Allo"
+            Name = "vlad", 
+            Surname = "vlad"
         });
-        Assert.NotNull(result);
-        Assert.Equal("1", result.Id);
+        Assert.NotNull(result.Name);
+
+    }
+
+    [Fact]
+    public async Task GetUserById__Test()
+    {
+        string id = "1";
+        var userRepositoryMock = new Mock<IUserRepository>();
+        userRepositoryMock.Setup(x => x.Get(It.IsAny<string>()))
+            .ReturnsAsync(new User(){Id = id});
+
+        var getUserHandler = new GetUserByIdHandler(userRepositoryMock.Object);
+
+        var result = await getUserHandler.Handle("1");
+        
+        Assert.Equal(id, result.Id);
     }
 }
